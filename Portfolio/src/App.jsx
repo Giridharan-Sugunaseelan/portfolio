c336022d9b95c451887853d886ca2c4373321434
Giridharan Sugunaseelan <144659110+Giridharan-Sugunaseelan@users.noreply.github.com>
2024-02-22 20:15:27 +0530

    Add files via upload

---
+++ Portfolio/src/App.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import "./App.css";
+import styled, { ThemeProvider } from "styled-components";
+import { darkTheme } from "./utils/Themes";
+import Navbar from "./components/NavBar/Navbar";
+import HeroSection from "./components/HeroSection/HeroSection";
+import Skills from "./components/Skills/Skills";
+import Education from "./components/Education/Education";
+import Projects from "./components/Projects/Projects";
+import Experience from "./components/Experience/Experience";
+import Contact from "./components/Contact/Contact";
+import { BrowserRouter } from "react-router-dom";
+
+const Body = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+  width: 100%;
+  overflow-x: hidden;
+`;
+
+const Wrapper = styled.div`
+  background: linear-gradient(
+      38.73deg,
+      rgba(204, 0, 187, 0.15) 0%,
+      rgba(201, 32, 184, 0) 50%
+    ),
+    linear-gradient(
+      141.27deg,
+      rgba(0, 70, 209, 0) 50%,
+      rgba(0, 70, 209, 0.15) 100%
+    );
+  width: 100%;
+  clip-path: polygon(0 0, 100% 0, 100% 100%, 30% 98%, 0 100%);
+`;
+
+function App() {
+  const [openModal, setOpenModal] = useState({ state: false, project: null });
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <BrowserRouter>
+        <Navbar />
+        <Body>
+          <HeroSection />
+          <Wrapper>
+            <Skills />
+            <Projects />
+            <Education />
+          </Wrapper>
+          <Experience />
+          <Contact />
+        </Body>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ Portfolio/src/data/constants.js
@@ -0,0 +1,237 @@
+import Calculator from "../img/Calculator.png";
+import WiseWallet from "../img/WiseWallet.png";
+import TodoApplication from "../img/Todo-Application.png";
+import EMS from "../img/EMS.png";
+
+export const Bio = {
+  name: "Giridharan Sugunaseelan",
+  roles: [
+    "Full Stack Developer",
+    "Java Developer",
+    "Mechanical Engineer",
+    "Programmer",
+  ],
+  description:
+    "Experienced engineer transitioning to software development, equipped with strong problem-solving abilities and technical proficiency. Excited to channel my engineering background into coding challenges. Adept at quick learning and fueled by a passion for innovation and collaboration. Seeking opportunities in dynamic software development environments to leverage my diverse skill set effectively.",
+  github: "https://github.com/Giridharan-Sugunaseelan",
+  resume:
+    "https://drive.google.com/file/d/16ZalP852-cgfuk4s3VXwm6IdeUUomc4n/view?usp=drive_link",
+  linkedin: "https://www.linkedin.com/in/giridharan-sugunaseelan/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Redux",
+        image:
+          "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Java",
+        image: "https://i.postimg.cc/gJPzjHNC/283001.png",
+      },
+      {
+        name: "Spring Framework",
+        image: "https://i.postimg.cc/nzBB62PM/1614306.png",
+      },
+      {
+        name: "Hibernate",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/hibernate/hibernate-original.svg",
+      },
+      {
+        name: "Spring Data JPA",
+        image: "https://i.postimg.cc/xdsyLQxQ/0-Juy-Oix-Yr-YWb6v6t-S.png",
+      },
+      {
+        name: "SpringBoot",
+        image: "https://i.postimg.cc/QtYyP4L1/1614368.png",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "Node Js",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://e7.pngegg.com/pngimages/713/558/png-clipart-computer-icons-pro-git-github-logo-text-logo-thumbnail.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "IntelliJ",
+        image: "https://i.postimg.cc/kXYrVvT2/1773394.png",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://static-00.iconduck.com/assets.00/postman-icon-497x512-beb7sy75.png",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://i.postimg.cc/cCYwDx88/668557.png",
+    role: "HVAC Draftsman",
+    company: "Blue Star Limited",
+    date: "October 2021 - September 2022",
+    desc: "Developing shop drawings quantity take off and collabrating with other services, site engineers and clients.",
+  },
+  {
+    id: 1,
+    img: "https://i.postimg.cc/xjhSM0R3/download.png",
+    role: "HVAC Design Engineer",
+    company: "DesignTree Service Consultants Pvt.Ltd",
+    date: "September 2022 - October 2023",
+    desc: "Designing ventilation and air-conditioning systems for residential and commercial buildings, co-ordinating with other services, attending meetings with client, site inspections, collabrating with vendors",
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://i.postimg.cc/pVqJ6T7K/download.jpg",
+    school: "Panimalar Institute of Technology",
+    date: "2016 - 2020",
+    grade: "63%",
+    degree: "Bachelor of Mechanical Engineering",
+  },
+  {
+    id: 1,
+    img: "https://i.postimg.cc/3wYZqvKC/unnamed.png",
+    school: "Laurel Higher Secondary School",
+    date: "2015 - 2016",
+    grade: "86.3%",
+    degree: "HSLC",
+  },
+  {
+    id: 2,
+    img: "https://i.postimg.cc/3wYZqvKC/unnamed.png",
+    school: "Laurel Higher Secondary School",
+    date: "2013 - 2014",
+    grade: "94.8%",
+    degree: "SSLC",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "WiseWallet- Expense Tracker",
+    date: "Jan 2024 - Jan 2024",
+    description:
+      "WiseWallet is an intutive expense tracking app that helps users manage their finances early.Users can manually input expenses and income, gaining a clear overview of their financial history. With a user-friendly interface and strong backend security, WiseWallet ensures secure and convinent tracking of financial activities.",
+    image: WiseWallet,
+    tags: [
+      "React Js",
+      "MySQL",
+      "Node Js",
+      "Redux",
+      "ChartJS",
+      "Bootstrap",
+      "SpringBoot",
+      "Spring Security",
+      "Spring Data JPA",
+    ],
+    github: "https://github.com/SGN31/WiseWallet-ExpenseTracker-Backend",
+  },
+  {
+    id: 2,
+    title: "Employee Management System",
+    date: "Dec 2023 - Dec 2023",
+    description:
+      "Designed, the employee management system integrates Spring Boot and React, offering essential CRUD functionality for efficient employee data management. Enhanced with Bootstrap, it delivers an impressive, responsible interface while ensuring the reliable data storage with MySQL.",
+    image: EMS,
+    tags: [
+      "ReactJS",
+      "Redux",
+      "ChartJS",
+      "Bootstrap",
+      "SpringBoot",
+      "Spring Data JPA",
+      "MySQL",
+    ],
+    github: "https://github.com/SGN31/Employee_Management_System",
+  },
+  {
+    id: 3,
+    title: "Todo Application",
+    date: "Dec 2023 - Dec2023",
+    description:
+      "A basic todo list application created using React,Tasks are stored locally in the browser memory, ensuring seamless user experience and accessability.",
+    image: TodoApplication,
+    tags: ["React", "CSS"],
+    github: "https://github.com/SGN31/Todo-Application",
+  },
+  {
+    id: 4,
+    title: "Calculator",
+    date: "Dec 2023 - Dec2023",
+    description:
+      "A simple calculator application built using HTML, CSS, and JavaScript. This calculator allows users to perform basic arithmetic operations such as addition, subtraction, multiplication, and division. It features a user-friendly interface with buttons for numbers and operations, as well as support for decimal numbers. The application provides real-time calculation results and includes a clear button to reset the input. With its minimalist design and intuitive functionality, this calculator offers a convenient tool for performing quick calculations on the web.",
+    image: Calculator,
+    tags: ["HTML", "CSS", "JavaScript"],
+    github: "https://github.com/SGN31/calculator.github.io",
+    link: "https://sgn31.github.io/calculator.github.io/",
+  },
+];
+
+export const contactIcons = {
+  github: "https://i.postimg.cc/rmRRnZ20/64439.png",
+
+  linkedin:
+    "https://raw.githubusercontent.com/devicons/devicon/master/icons/linkedin/linkedin-original.svg",
+};
